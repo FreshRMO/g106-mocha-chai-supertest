219ffab702d1c6c715b95b02060a3331ef0c2051
Alicia Key <alicia.key@galvanize.com>
2018-12-02 17:11:28 -0700

    My programming rules compliant

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var app = express();
+const indexRouter = require('./routes/index')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+const app = express()
 
-app.use('/', indexRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-module.exports = app;
+app.use('/', indexRouter)
+
+module.exports = app
