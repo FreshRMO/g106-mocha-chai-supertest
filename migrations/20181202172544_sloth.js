cf38fe3d5504145b364f984e3196ed74ff087928
Alicia Key <alicia.key@galvanize.com>
2018-12-02 18:14:19 -0700

    Change sloth to sloths

--- migrations/20181202172544_sloth.js
+++ migrations/20181202172544_sloth.js
@@ -1,10 +1,10 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('sloth', function(table) {
+  return knex.schema.createTable('sloths', function(table) {
     table.increments()
     table.string('name')
   })
 }
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('sloth')
+  return knex.schema.dropTable('sloths')
 }
--- seeds/001_sloth.js
+++ seeds/001_sloth.js
@@ -1,15 +1,15 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('sloth').del()
+  return knex('sloths').del()
     .then(function () {
       // Inserts seed entries
-      return knex('sloth').insert([
+      return knex('sloths').insert([
         {id: 1, name: 'Dagny'},
         {id: 2, name: 'Ellis'},
         {id: 3, name: 'Wyatt'}
       ])
       .then(() => {
-        return knex.raw("SELECT setval('sloth_id_seq', (SELECT MAX(id) FROM sloth))")
+        return knex.raw("SELECT setval('sloths_id_seq', (SELECT MAX(id) FROM sloths))")
       })
     })
 }
