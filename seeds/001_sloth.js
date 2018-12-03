5ec604275b5a1c8ad6461eaa6fa25d558790d1b4
Alicia Key <alicia.key@galvanize.com>
2018-12-02 18:17:30 -0700

    Disable sloth seeding

--- seeds/001_sloth.js
+++ seeds/001_sloth.js
@@ -1,15 +1,17 @@
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('sloths').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('sloths').insert([
-        {id: 1, name: 'Dagny'},
-        {id: 2, name: 'Ellis'},
-        {id: 3, name: 'Wyatt'}
-      ])
-      .then(() => {
-        return knex.raw("SELECT setval('sloths_id_seq', (SELECT MAX(id) FROM sloths))")
-      })
-    })
-}
+// Since this is just to show off testing, we are not seeding here.
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('sloths').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('sloths').insert([
+//         {id: 1, name: 'Dagny'},
+//         {id: 2, name: 'Ellis'},
+//         {id: 3, name: 'Wyatt'}
+//       ])
+//       .then(() => {
+//         return knex.raw("SELECT setval('sloths_id_seq', (SELECT MAX(id) FROM sloths))")
+//       })
+//     })
+// }
